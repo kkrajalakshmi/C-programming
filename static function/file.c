2ff9dcc0fb92f87fae8ae5cd103d2870dfe510a2
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-08-17 12:20:43 +0530

    Add files via upload

---
+++ static function/file.c
@@ -0,0 +1,6 @@
+//static is used to stop accessing the file from outside
+
+static int sum(int x,int y)
+{
+    return (x+y);
+}
---
+++ static function/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int sum(int,int);
+int main()
+{
+    int a=2,b=4,result;
+    result=sum(a,b);
+    printf("The addition of two integers is : %d\n",result);
+    return 0;
+}
